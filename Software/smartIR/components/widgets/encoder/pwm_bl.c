f5aef4783419a9e47e553f11013913caabdcf979
bosco <1270112821@qq.com>
2022-11-09 14:50:56 +0800

    code format

--- Software/smartIR/components/widgets/encoder/pwm_bl.c
+++ Software/smartIR/components/widgets/encoder/pwm_bl.c
@@ -1,6 +1,5 @@
 #include "pwm_bl.h"
 
-#include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "soc/ledc_periph.h"
 #include <stdio.h>
@@ -12,8 +11,70 @@ typedef struct {
     bool pwm_control; // true: LEDC is used, false: GPIO is used
     int index;        // Either GPIO or LEDC channel
 } pwm_bl_t;
+pwm_bl_t bl;
+
+void pwm_bl_init(gpio_num_t pin, bool output_invert, bool pwm_control){
+    if(!GPIO_IS_VALID_OUTPUT_GPIO(pin)){
+        ESP_LOGW(TAG, "Invalid GPIO");
+        return;
+    }
+
+    if(pwm_control){
+        bl.pwm_control = true;
+        bl.index = 0;
+        const ledc_channel_config_t LCD_backlight_channel = {
+            .gpio_num = pin,
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = 0,
+            .intr_type = LEDC_INTR_DISABLE,
+            .timer_sel = 0,
+            .duty = 0,
+            .hpoint = 0
+        };
+        const ledc_timer_config_t LCD_backlight_timer = {
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .bit_num = LEDC_TIMER_10_BIT,
+            .timer_num = 0,
+            .freq_hz = 5000,
+            .clk_cfg = LEDC_AUTO_CLK
+        };
+        ESP_ERROR_CHECK(ledc_timer_config(&LCD_backlight_timer));
+        ESP_ERROR_CHECK(ledc_channel_config(&LCD_backlight_channel));
+        gpio_matrix_out(pin, ledc_periph_signal[LEDC_LOW_SPEED_MODE].sig_out0_idx + 0, output_invert, 0);
+    }else {
+        bl.index = pin;
+        gpio_pad_select_gpio(pin);
+        ESP_ERROR_CHECK(gpio_set_direction(pin, GPIO_MODE_OUTPUT));
+        gpio_matrix_out(pin, SIG_GPIO_OUT_IDX, output_invert, false);
+    }
+}
 
 
+void pwm_bl_set(int brightness_percent){
+    if(brightness_percent > 100) brightness_percent = 100;
+    if(brightness_percent < 0) brightness_percent = 0;
+
+    ESP_LOGI(TAG, "Setting LCD backlight: %d%%", brightness_percent);
+
+    if(bl.pwm_control){
+        uint32_t duty_cycle = (1023 * brightness_percent) / 100; // LEDC resolution set to 10bits, thus: 100% = 1023
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, bl.index, duty_cycle));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, bl.index));
+    }else {
+        ESP_ERROR_CHECK(gpio_set_level(bl.index, brightness_percent));
+    }
+}
+
+void pwm_bl_deinit(void){
+    if(bl.pwm_control){
+        ledc_stop(LEDC_LOW_SPEED_MODE, bl.index, 0);
+    }else{
+        gpio_reset_pin(bl.index);
+    }
+}
+
+
+#if 0
 pwm_bl_handle_t pwm_bl_create(const pwm_bl_config_t *config){
     if(config == NULL) return NULL;
     if(!GPIO_IS_VALID_OUTPUT_GPIO(config->gpio_num)){
@@ -87,3 +148,4 @@ void pwm_bl_delete(pwm_bl_handle_t bl) {
     }
     free (bl);
 }
+#endif
--- Software/smartIR/components/widgets/encoder/pwm_bl.h
+++ Software/smartIR/components/widgets/encoder/pwm_bl.h
@@ -2,8 +2,9 @@
 #define _PWM_BL_H_
 
 #include <stdbool.h>
+#include "driver/gpio.h"
 
-typedef void * pwm_bl_handle_t;
+/* typedef void * pwm_bl_handle_t;
 
 typedef struct {
     bool pwm_control;
@@ -13,12 +14,20 @@ typedef struct {
     int timer_idx;
     int channel_idx;
 }pwm_bl_config_t;
+ */
 
+void pwm_bl_init(gpio_num_t pin, bool output_invert, bool pwm_control);
+void pwm_bl_deinit(void);
+void pwm_bl_set(int brightness_percent);
+
+#if 0
 pwm_bl_handle_t pwm_bl_create(const pwm_bl_config_t *config);
 
 void pwm_bl_set(pwm_bl_handle_t bl, int brightness_percent);
 
 void pwm_bl_delete(pwm_bl_handle_t bl);
+
+#endif
 
 
 #endif
